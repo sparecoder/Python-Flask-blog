25ff7a40d8ff27ee81e9779e06032f0110563e3b
sparecoder <146880712+sparecoder@users.noreply.github.com>
2024-05-17 14:41:28 +0530

    coding_ninjas.sql

--- coding_ninjas.sql
+++ coding_ninjas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2024 at 04:01 PM
+-- Generation Time: May 17, 2024 at 11:09 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -55,7 +55,8 @@ INSERT INTO `contacts` (`S.no`, `Name`, `phone_num`, `msg`, `date`, `email`) VAL
 (12, 'ani Singh', '08005570859', 'heyy', '2023-08-26 19:52:12', 'dityasingh342@gmail.com'),
 (13, 'Bhawani Singh', '08005570859', 'shbdsdodpoj', '2023-09-29 02:00:09', 'vikramadityasingh342@gmail.com'),
 (14, 'Bhawani Singh', '08005570859', 'Hii', '2023-10-26 18:50:01', 'vikramadityasingh342@gmail.com'),
-(15, 'Bhawani Singh', '08005570859', 'Chumki', '2023-11-14 19:09:40', 'vikramadityasingh342@gmail.com');
+(15, 'Bhawani Singh', '08005570859', 'Chumki', '2023-11-14 19:09:40', 'vikramadityasingh342@gmail.com'),
+(16, 'Chumki Sur', '08005570859', 'HIIII', '2024-03-13 21:17:19', 'chumkisur@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`sno`, `title`, `tagline`, `slug`, `content`, `img_file`, `date`) VALUES
-(1, 'Tuple', 'Tuple in maths', 'new-post', 'In mathematics, a tuple is an ordered sequence of values. The values can be repeated, but their number is always finite. A tuple is often represented by a comma-delimited list whose values are enclosed in parentheses, although they\'re sometimes enclosed in square brackets or angle brackets.', 'post-bg.jpg', '2023-11-08 17:16:24'),
+(1, 'Flask', 'This website is made using Flask.', 'new-post', 'I have made this website using Flask, Flask is a lightweight web framework for building and deploying web applications in Python. This website will be explaining my projects in the field of Artificial Intelligence and Machine Learning, browse through my portfolio to discover a diverse range of projects. Each project is a testament to my passion for coding and AiMl.\r\n\r\nOne of the standout features of this website is the Contact Me button, conveniently placed in the top right corner for easy access. Whether you have a question, want to collaborate on a project, or simply want to say hello, I encourage you to reach out. I\'m always eager to connect with coding enthusiasts, potential clients, or anyone interested in discussing exciting opportunities.', 'flask.jpg', '2024-03-26 15:13:57'),
 (2, 'Stock market', 'A stock market, equity market, or share market', 'post-2', 'A stock market, equity market, or share market is the aggregation of buyers and sellers of stocks (also called shares), which represent ownership claims on businesses; these may include securities listed on a public stock exchange as well as stock that is only traded privately, such as shares of private companies that are sold to investors through equity crowdfunding platforms. Investments are usually made with an investment strategy in mind.', 'post-bg.jpg', '2024-01-20 19:44:34'),
 (3, 'Theodore II Laskaris', 'Theodore II Laskaris (1221/1222–1258) was a Byzantine emperor', 'post-3', 'Theodore II Laskaris (1221/1222–1258) was a Byzantine emperor who ruled the Empire of Nicaea from 1254 until his death. Nicaea was a successor state to the Byzantine Empire, after crusaders captured the Byzantine capital, Constantinople, in 1204 during the Fourth Crusade. Theodore began to write treatises on theological, historical and philosophical themes in his youth. From around 1242, he was co-ruler with his father, and his relationship with some prominent aristocrats grew tense. Succeeding his father in 1254 Theodore replaced many aristocratic officials and generals with loyalists, including some of low birth. In 1256, he repelled a Bulgarian invasion of Thrace and Macedonia and forced Epirus to cede Dyrrachium on the Adriatic Sea. Epirus allied with Serbia and Sicily, and jointly invaded Nicaea in 1257; Nicaea\'s new generals could not resist the invasion.', 'post-bg.jpg', '2024-01-20 19:45:48'),
 (4, 'The Little Girl in Blue', 'The Little Girl in Blue is an oil painting on canvas created in India', 'post-4', 'The Little Girl in Blue is an oil painting on canvas created in India by Hungarian-born Indian artist Amrita Sher-Gil in 1934. Under India\'s Antiquities and Art Treasures Act (1972) the work is a national art treasure and must stay in the country. In 2018, it was auctioned by Sotheby\'s, Mumbai, fetching US$2.67 million.\r\nSher-Gil spent her childhood in Hungary and India, and her later teens in France, before returning to India towards the end of 1934', 'post-bg.jpg', '2024-01-20 19:47:08'),
@@ -110,7 +111,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `S.no` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `S.no` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `posts`
